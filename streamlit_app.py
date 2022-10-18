497914924acbceece3d3e6faf3afb947ea57e4a0
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-18 18:16:16 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,17 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
+
+#dont run anything past here while we troubleshoot
+streamlit.stop()
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected=streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
@@ -40,13 +48,6 @@ except URLError as e:
   streamlit.error()
 
 
-#dont run anything past here while we troubleshoot
-streamlit.stop()
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
 
 
 streamlit.header("The fruit load list contains:")
