69a70bbeaa30c7d92e15667aeeb26eee62dfacd9
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-19 09:43:58 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 #dont run anything past here while we troubleshoot
-streamlit.stop()
+
 import streamlit
 import requests
 import snowflake.connector
