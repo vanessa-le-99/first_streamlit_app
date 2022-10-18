fa215acfd1eceac2529009bb1f7edc9eb3b229ac
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-19 09:27:55 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
+
+
+#Let's put a pick list here so they can pick the fruit they want to include
+fruits_selected=streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 #dont run anything past here while we troubleshoot
 streamlit.stop()
 import streamlit
@@ -20,11 +25,6 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
-#Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected=streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show=my_fruit_list.loc[fruits_selected]
-
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
