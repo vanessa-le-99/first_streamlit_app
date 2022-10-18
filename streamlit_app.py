43c31dfd578c2ddc2fbd0baf38432604ad9dfd0c
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-19 09:28:45 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
+import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
@@ -21,7 +21,6 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 #dont run anything past here while we troubleshoot
 streamlit.stop()
 import streamlit
-import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
