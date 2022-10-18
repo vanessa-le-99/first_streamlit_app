a5265ba391d1630ef9523b2ecd884fe59653131f
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-19 09:43:29 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,3 +66,9 @@ streamlit.write('Thanks for adding', fruit_add)
 
 #This will not work correctly
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+#Allow the end user to add a fruit to the list
+def inseret_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("insert into fruit_load_list_values ('"+ new_fruit+ "')")
+       return "Thanks for adding "+ new_fruit
