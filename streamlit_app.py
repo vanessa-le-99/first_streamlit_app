26f202b2956c5d5ae68b1f0d2dbe192dfd523edc
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-21 21:38:52 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 
