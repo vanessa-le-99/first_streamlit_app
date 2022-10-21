1d4624796ffad2cee88b307b2aaa040d0e0b78f5
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-21 21:29:34 +1100

    Update streamlit_app.py
    9- callign the fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,9 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
