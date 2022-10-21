b4ac1b33c6d7b3bd68634fc432f5fbf5a2d2725d
vanessa-le-99 <115606177+vanessa-le-99@users.noreply.github.com>
2022-10-21 21:30:44 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+streamlit.text(fruityvice_response.json())
 
+
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
